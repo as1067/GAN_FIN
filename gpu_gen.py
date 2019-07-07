52245709dde894d3903fee0c3aafea993cff10cb
Cyrus Tau <cyrus.tau@gmail.com>
2019-07-06 19:43:36 -0700

    fix activations gen

--- gpu_gen.py
+++ gpu_gen.py
@@ -576,7 +576,7 @@ class PM:
 
         # generator of GANs.
         def generator(gw1, gw2, reconstucted_network_adjacency_matrix, noise_z):
-            hidden = tf.tanh(tf.matmul(noise_z, reconstucted_network_adjacency_matrix * (gw1 * tf.transpose(gw1))))
+            hidden = tf.nn.leaky_relu(tf.matmul(noise_z, reconstucted_network_adjacency_matrix * (gw1 * tf.transpose(gw1))))
             output = tf.asinh(tf.matmul(hidden,reconstucted_network_adjacency_matrix*(gw2*tf.transpose(gw2))))
             return output
 
